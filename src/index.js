41c49b8c6e0a152752044ccd58ad12c301391a30
val <86112155+val1680@users.noreply.github.com>
2022-05-10 10:43:14 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,56 @@
+class Board extends React.Component {
+  renderSqare(i) {
+   return <Sqare value={i} />; 
+  }
+  render(){
+   const status = 'next player: X';
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board.row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board.row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+class Sqare extends React.Component {
+ render() {
+  return(
+    <button className="sqare">
+    {this.props.value}
+    </button>
+    );
+ }
+}
+class Game extends React.Component {
+  render (){
+   return(
+    <div className="game">
+     <div className="game-board">
+      <Board />
+     </div>
+     <div className="game-info">
+      <div>{/* status */}</div>
+      <ol>{/* TODO */}</ol>
+     </div>
+    </div>
+   )
+  }
+}
+// =======================
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
